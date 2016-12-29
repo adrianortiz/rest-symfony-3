25198902ed439746026110fa3cb20a99fe2c8966
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-29 15:42:44 -0600

    Get all users test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,18 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
+
+    /**
+     * @Route("/pruebas", name="homepage")
+     * @param Request $request
+     */
+    public function pruebasAction(Request $request)
+    {
+        // Trabajar con entidades de la BD
+        $em = $this->getDoctrine()->getManager();
+        // Obtener registros de Usuarios
+        $users = $em->getRepository('BackendBundle:User')->findAll();
+        var_dump($users);
+        die();
+    }
 }
